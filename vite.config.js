c3709fae0c98e0d8e666cfa75caa07c7f280d7eb
AngelNelly <talktoangellnelly@gmail.com>
2025-06-30 07:50:28 +0100

    🛠️ Final updates before deployment

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,14 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()],
+    base: "week-3-react-js-assignment-AngelNelly,"
 });
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // 👇 Replace with your repo name
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/your-repo-name/", 
+// })
