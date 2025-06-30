13dc81aafb9f6e650980d4ae1d4368df2566d639
AngelNelly <talktoangellnelly@gmail.com>
2025-06-30 13:29:18 +0100

    ✅ Configured for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()],
-    base: "/",
-    // base: '/week-3-react-js-assignment-AngelNelly/',
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  // base: '/week-3-react-js-assignment-AngelNelly/',
 });
-
